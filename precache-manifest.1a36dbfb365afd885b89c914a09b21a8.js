be709b2634a9daa7aa0b2b50984faa54deec8c59
tchelet1 <tchelet.sinay@gmail.com>
2020-02-16 19:22:06 +0200

    Updates

---
+++ precache-manifest.1a36dbfb365afd885b89c914a09b21a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04ecdb3541b9b207c38c23f786040ec7",
+    "url": "/smartHome/index.html"
+  },
+  {
+    "revision": "d4b767db18c6b00a652f",
+    "url": "/smartHome/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "40b3394529c6d03f2117",
+    "url": "/smartHome/static/css/main.22a37aa0.chunk.css"
+  },
+  {
+    "revision": "d4b767db18c6b00a652f",
+    "url": "/smartHome/static/js/2.295fc6ee.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/smartHome/static/js/2.295fc6ee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "40b3394529c6d03f2117",
+    "url": "/smartHome/static/js/main.2d6c453e.chunk.js"
+  },
+  {
+    "revision": "7b8993db1aeeba91f0a2",
+    "url": "/smartHome/static/js/runtime-main.ed08de80.js"
+  }
+]);
